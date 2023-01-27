16b746f3df02cc3c55f415ddb79aaf4fe9861650
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-01-27 11:54:14 +0530

    fix: update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,12 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Tailwind Next Eslint Boilerplate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <h1 className="text-6xl font-bold">Tailwind Next Eslint Boilerplate</h1>
 
         <p className="mt-3 text-2xl">
           Get started by editing{' '}
